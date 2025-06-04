aa8ddf8b04f4f3c49186ffc840753fdbac4ffd18
Yongyi Chen <yongyi781@outlook.com>
2025-06-03 22:46:05 -0400

    Upgrade to Tailwind 4

--- astro.config.ts
+++ astro.config.ts
@@ -3,16 +3,23 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 
 import svelte from "@astrojs/svelte"
-import tailwind from "@astrojs/tailwind"
 import remarkMath from "remark-math"
 import rehypeKatex from "rehype-katex"
+
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yongyi781.github.io",
+
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
   },
-  integrations: [mdx(), sitemap(), svelte(), tailwind()]
+
+  integrations: [mdx(), sitemap(), svelte()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
